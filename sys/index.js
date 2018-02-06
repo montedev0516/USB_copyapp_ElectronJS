88d6d8e4757dfbd8c9037adce4febf38753ccce8
montedev0516 <montecristodev0516@gmail.com>
2018-02-05 20:40:01 -0500

    added stub of js renderer

--- sys/index.js
+++ sys/index.js
@@ -1,3 +1,25 @@
-const electron = require('electron')
+const electron = require('electron');
+const app = electron.app;
+
+const path = require('path');
+const url = require('url');
 
 console.log('EveryUSB Constent System starting');
+
+let mainWindow;
+
+function createWindow() {
+    mainWindow = new electron.BrowserWindow();
+
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'evusb.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    mainWindow.on('closed', function() {
+        mainWindow = null;
+    });
+}
+
+app.on('ready', createWindow);
