701a72ba5387042c453e331f5223d1c20827d952
montedev0516 <montecristodev0516@gmail.com>
2018-02-06 17:06:46 -0500

    implemented file getter, prep for decryption function

--- sys/server.js
+++ sys/server.js
@@ -4,6 +4,7 @@
 var cfg = require('./config.json');
 
 const express = require('express');
+const fs = require('fs');
 var app = express();
 
 app.get('/status', function(req, res) {
@@ -13,7 +14,22 @@ app.get('/status', function(req, res) {
     });
 });
 
-app.listen(cfg.SERVER_PORT, function() {
+app.get('/x', function(req, res) {
+    var fname = './content/' + req.query.f; // TODO now: sanitize this filename
+    console.log('decrypt: ' + fname);
+    fs.readFile(fname, (err, data) => {
+        if (err) {
+            console.log('READ ERROR: ' + err);
+            res.sendStatus(404);
+        } else {
+            res.set('Content-Type', 'video/mp4');
+            res.send(data);
+        }
+    });
+
+});
+
+app.listen(cfg.SERVER_PORT, () => {
     console.log('Listening on ' + cfg.SERVER_PORT);
 });
 
