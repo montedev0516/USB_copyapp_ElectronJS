dc50602aa5a9689318889dc03cb47215dc7bbafb
montedev0516 <montecristodev0516@gmail.com>
2018-02-08 17:21:56 -0500

    implemented session locking

--- sys/index.js
+++ sys/index.js
@@ -3,13 +3,25 @@ const app = electron.app;
 
 const path = require('path');
 const url = require('url');
+const uuidv4 = require('uuid/v4');
 
 console.log('EveryUSB Constent System starting');
 
+// start the server
+const server = require('./server.js');
+
 let mainWindow;
+let sessionId = uuidv4();
 
 function createWindow() {
     mainWindow = new electron.BrowserWindow();
+
+    mainWindow.webContents.session.webRequest.onBeforeSendHeaders((details, callback) => {
+        details.requestHeaders['x-api-key'] = 'whatwhat'; //secret
+        details.requestHeaders['session-id'] = sessionId;
+        callback({cancel:false, requestHeaders: details.requestHeaders});
+    });
+    server.lockSession(sessionId, mainWindow.webContents.session.getUserAgent());
 
     mainWindow.loadURL(url.format({
         pathname: path.join(__dirname, 'evusb.html'),
@@ -26,5 +38,3 @@ function createWindow() {
 
 app.on('ready', createWindow);
 
-// start the server
-require('./server.js');
--- sys/server.js
+++ sys/server.js
@@ -8,42 +8,71 @@ const fs = require('fs');
 const pwsys = require('./src/password');
 const crypto = require('crypto');
 
-var app = express();
+var _uuid = 'c02bb8c7-8cca-4805-9711-0545a471103f'; //null;
+var _agent = null;
 
-app.get('/status', function(req, res) {
-    res.json({
-        "running": true,
-        "status": "running"
-    });
-});
+var app = express();
 
 // Get the random bytes buffer used for encryption.  This /could/
 // also be encrypted, but it would be trivially easy to get the secret
 // to decrypt it, so why?
 var bytes = fs.readFileSync('bytes.dat');
 
+function isValid(av) {
+    [req, res] = av;
+    if (_uuid == null || _agent == null) {
+        // disallow connection if server not set up
+        res.sendStatus(500);
+        return false;
+    }
+
+    if (!(req.get('user-agent') === _agent &&
+          req.get('session-id') === _uuid)) {
+        // disallow connection from external browser
+        res.sendStatus(401);
+        return false;
+    }
+
+    return true;
+}
+
+app.get('/status', function(req, res) {
+    if (!isValid([req, res])) { return; }
+
+    res.json({
+        "running": true,
+        "status": "running"
+    });
+});
+
 app.get('/x', function(req, res) {
-    var fname = './content/' + req.query.f; // TODO now: sanitize this filename
-    var type = req.query.t;
+    if (!isValid([req, res])) { return; }
+
+    let fname = './content/' + req.query.f; // TODO now: sanitize this filename
+    let type = req.query.t;
+    let key = req.get('x-api-key');
     console.log('decrypt: ' + fname);
     fs.readFile(fname, (err, data) => {
         if (err) {
             console.log('READ ERROR: ' + err);
             res.sendStatus(404);
         } else {
-            var cipher = crypto.createDecipher(
+            let decipher = crypto.createDecipher(
                 'aes-256-cbc',
-                pwsys.makePassword('123','1.2','whatever','whatwhat', bytes)
+                pwsys.makePassword('123','1.2','whatever', key, bytes)
             );
 
-            var decrypted = Buffer.concat(
-                [cipher.update(data), cipher.final()]
-            );
-            console.log('data size: ' + data.length);
-            console.log('decrypt size: ' + decrypted.length);
+            try {
+                let decrypted = Buffer.concat(
+                    [decipher.update(data), decipher.final()]
+                );
 
-            res.set('Content-Type', type);
-            res.send(decrypted);
+                res.set('Content-Type', type);
+                res.send(decrypted);
+            } catch (err) {
+                console.log('DECRYPT ERROR: ' + err);
+                res.sendStatus(404);
+            }
         }
     });
 
@@ -53,3 +82,7 @@ app.listen(cfg.SERVER_PORT, () => {
     console.log('Listening on ' + cfg.SERVER_PORT);
 });
 
+exports.lockSession = (uuid, agent) => {
+    _uuid = uuid;
+    _agent = agent;
+};
