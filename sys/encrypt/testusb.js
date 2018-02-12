ce3561bb7d7abb23bb2cb0ce078c1fed41a9fb8c
montedev0516 <montecristodev0516@gmail.com>
2018-02-12 13:25:46 -0500

    added test script that can be run through electron

--- sys/encrypt/testusb.js
+++ sys/encrypt/testusb.js
@@ -5,13 +5,13 @@ var usb = require('usb');
 var devices = usb.getDeviceList();
 
 for (var d of devices) {
-    if (d.deviceDescriptor.idVendor = 0x058f) { 
+    if (d.deviceDescriptor.idVendor == 0x058f) {
         let dd = d.deviceDescriptor;
         console.log('found at '+ d.busNumber + ':' + d.deviceAddress);
         console.log('----');
-        console.log('found vid:pid \t= ' + 
+        console.log('found vid:pid \t= ' +
                     dd.idVendor.toString(16) + ':' + dd.idProduct.toString(16));
-        console.log('mfg:prd:ser   \t= ' + dd.iManufacturer + ':' + 
+        console.log('mfg:prd:ser   \t= ' + dd.iManufacturer + ':' +
                     dd.iProduct + ':' + dd.iSerialNumber);
 
         var descs = [];
@@ -20,7 +20,7 @@ for (var d of devices) {
         function readDesc(idx) {
             return new Promise((resolve, reject) => {
                 d.getStringDescriptor(idx, (err, data) => {
-                    if (err) { 
+                    if (err) {
                         console.log('err  ' + err);
                         throw err;
                     }
@@ -33,8 +33,10 @@ for (var d of devices) {
 
         readDesc(1)
             .then(() => {return readDesc(2)})
-            .then(() => {return readDesc(3)});
+            .then(() => {return readDesc(3)})
+            .then(() => process.exit(0));
 
         break;
     }
 }
+
