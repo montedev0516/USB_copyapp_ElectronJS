d8f1f82c38fdac8cac66d0b7199b4f37128a6619
montedev0516 <montecristodev0516@gmail.com>
2018-02-22 13:44:13 -0500

    fixes to the package script

--- sys/package.sh
+++ sys/package.sh
@@ -11,9 +11,10 @@ tar cfJ out/content.tar.xz content/ bytes.dat .hidfil.sys
 
 mkdir out/dist && cd out/dist
 
-cp -r ../../node_modules .
+cp -L -r ../../node_modules .
 mv node_modules/electron/dist electron.linux
 rm -r node_modules/electron*
+rm -r node_modules/.bin
 cp ../../scripts/* .
 tar xvf ../content.tar.xz
 cp ../../*.js ../../evusb.html ../../config.json .
