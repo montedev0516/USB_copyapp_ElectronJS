52c66486fba45cf1b15c8bb64f4abafaa60f813b
montedev0516 <montecristodev0516@gmail.com>
2018-02-27 11:58:27 -0500

    committed after build on windows

--- sys/rebuild-module.sh
+++ sys/rebuild-module.sh
@@ -1,1 +1,2 @@
-../../sys/node_modules/.bin/node-gyp rebuild --target=1.8.2 --arch=x64 --dist-url=https://atom.io/download/electron
+#../../sys/node_modules/.bin/node-gyp rebuild --target=1.8.2 --arch=x64 --dist-url=https://atom.io/download/electron
+../../sys/node_modules/.bin/node-gyp rebuild --target=1.8.2 --arch=ia32 --dist-url=https://atom.io/download/electron
