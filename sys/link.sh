2fa9c9072109b5c7c2a321f1e585ca5abb3fe4eb
montedev0516 <montecristodev0516@gmail.com>
2018-02-27 12:42:48 -0500

    more windows build stuff

--- sys/link.sh
+++ sys/link.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
-OS=`uname -s`
+OS=`/usr/bin/uname -s`
 
 if [ $OS = "Linux" ] ; then
     link=node_modules/usb-detection.linux
 elif [ $OS = "Darwin" ] ; then
     link=node_modules/usb-detection.darwin
 else
+    export CYGWIN=winsymlinks:nativestrict
     link=node_modules/usb-detection.win32
 fi
 
 if [ ! -L $link ] ; then 
-    ln -vs usb-detection $link
+    /usr/bin/ln -vs usb-detection $link
 fi
--- sys/package.sh
+++ sys/package.sh
@@ -9,6 +9,11 @@ tar cfJ out/content.tar.xz content/ bytes.dat .hidfil.sys
 rm node_modules/file-browser node_modules/usb-detection*
 rm -f package-lock.json
 
-electron-forge package
-cp -r ../repo/file-browser ./out/everyusb-linux-x64/resources/app/node_modules/
-cp -r ../repo/node-usb-detection ./out/everyusb-linux-x64/resources/app/node_modules/usb-detection.linux
+cmd /c electron-forge package
+if [ -d ./out/everyusb-linux-x64 ] ; then
+    cp -r ../repo/file-browser ./out/everyusb-linux-x64/resources/app/node_modules/
+    cp -r ../repo/node-usb-detection ./out/everyusb-linux-x64/resources/app/node_modules/usb-detection.linux
+else
+    cp -r ../repo/file-browser ./out/everyusb-win32-ia32/resources/app/node_modules/
+    cp -r ../repo/node-usb-detection ./out/everyusb-win32-ia32/resources/app/node_modules/usb-detection.win32
+fi
