7483ab7ac6357a4046062fb64643ce3277810f44
montedev0516 <montecristodev0516@gmail.com>
2018-02-28 10:00:57 -0500

    use local packed tgz files instead of symlinks
    Use local packed tgz files instead of symlinks.
    This works better with the npm pruning system, which doesn't
    handle ".." symlinks in node_modules very well.

--- sys/package.sh
+++ sys/package.sh
@@ -6,12 +6,9 @@ set -x
 
 tar cfJ out/content.tar.xz content/ bytes.dat .hidfil.sys
 
-rm -f node_modules/file-browser node_modules/usb-detection*
-rm -f package-lock.json
-
 ( cd default_app ; asar pack . ../default_app.asar )
 
-electron-forge package
+electron-forge package || exit
 if [ -d ./out/everyusb-linux-x64 ] ; then
     dir=everyusb-linux-x64
     suffix=linux
@@ -20,8 +17,7 @@ else
     suffix=win32
 fi
 
-cp -r ../repo/file-browser ./out/$dir/resources/app/node_modules/
-cp -r ../repo/node-usb-detection ./out/$dir/resources/app/node_modules/usb-detection.$suffix
+mv ./out/$dir/resources/app/node_modules/usb-detection ./out/$dir/resources/app/node_modules/usb-detection.$suffix
 
 cd ./out/$dir
 mv resources ..
