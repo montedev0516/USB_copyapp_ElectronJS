f060ca370e280ebf4b3594e9780be8b391b323c8
montedev0516 <montecristodev0516@gmail.com>
2018-04-04 13:54:22 -0400

    copy the win drive data if on that system

--- sys/copy.sh
+++ sys/copy.sh
@@ -11,6 +11,8 @@ cd `dirname $0`/..
 app="resources/app/es6-shim.js resources/app/node_modules resources/app/package.json resources/app/package-lock.json resources/app/src resources/app/.cache"
 if [ `uname -s` = "Linux" ] ; then
     drive="usbcopypro-linux-x64"
+elif [ `uname -s` = "CYGWIN_NT-6.1" ] ; then
+    drive="usbcopypro-win32-ia32"
 fi
 
 mkdir app/sys
@@ -18,12 +20,13 @@ mkdir app/sys
 pushd sys/dist/out
 tar cf - $app | pv | ( cd ../../../app/sys ; tar xf - )
 
+mkdir ../../../drive
+cp -v resources/app/locator.json ../../../drive/
+
 if [ -n "$drive" ] ; then
     mkdir -p ../../../drive/sys
     cp -r $drive ../../../drive/sys/
 
     # OSX is manual
-
-    # win32 is manual
 fi
 
