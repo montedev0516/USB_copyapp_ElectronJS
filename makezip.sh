0fbed507c17011eae56e16ecfaed14f9997656d1
montedev0516 <montecristodev0516@gmail.com>
2018-04-09 10:23:16 -0400

    added creation of the encryption tool in zipfile

--- makezip.sh
+++ makezip.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #
+# encrypt: encryption tool
 # shared: shared content files
 # app: platform-independent (mostly) app files
 # drive: platform-specific binaries for electron
@@ -13,9 +14,17 @@ cat > drive/locator.json <<EOT
 {
     "shared": "./shared",
     "app": "./app/sys/resources/app",
-    "drive": ".\\drive\\sys\\usbcopypro-win32-ia32\\usbcopypro.exe"
+    "drive": ".\\\\drive\\\\sys\\\\usbcopypro-win32-ia32\\\\usbcopypro.exe"
 }
 EOT
+
+if [ -d ./sys/encrypt/out ] ; then
+    pushd sys/encrypt/out  > /dev/null 2>&1 || exit
+    FILE=${tag}-encrypt.zip
+    echo Writing $FILE
+    zip -r ../../../$FILE . | pv > /dev/null
+    popd > /dev/null 2>&1
+fi
 
 for d in $DIRS ; do
     [ -d $d ] || continue
