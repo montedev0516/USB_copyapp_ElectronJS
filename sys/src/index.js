c71a621bcbdf7c1deef60155b6ce2ce1817065c3
montedev0516 <montecristodev0516@gmail.com>
2018-07-20 14:44:09 -0400

    eslint on index.js

--- sys/src/index.js
+++ sys/src/index.js
@@ -1,6 +1,3 @@
-const electron = require('electron');
-const app = electron.app;
-app.commandLine.appendSwitch('ignore-certificate-errors');
 
 const path = require('path');
 const url = require('url');
@@ -9,41 +6,42 @@ const fs = require('fs');
 const opn = require('opn');
 const Worker = require('tiny-worker');
 
+const electron = require('electron');
+
+const { app } = electron;
+app.commandLine.appendSwitch('ignore-certificate-errors');
+
 let mainWindow;
 let workerThread;
 const sessionId = uuidv4();
+
+/* eslint-disable no-restricted-globals */
 
 function createServerWorker(pserverjs, plocator, psessionId, puserAgent) {
     const worker = new Worker(() => {
         let server;
 
-        function keepAlive() {
-            self.postMessage(process.pid);
-            (function keepAliveSub(){
-                if (!server || server.keepAlive) {
-                    setTimeout(keepAliveSub, 1000);
-                }
-            })();
-        }
-
-        self.onmessage = function(event) {
+        /* global self */
+        self.onmessage = (event) => {
             try {
+                // eslint-disable-next-line global-require, import/no-dynamic-require
                 server = require(event.data.serverjs);
                 server.configure(event.data.locator);
-                server.lockSession(event.data.sessionId,
-                                   event.data.userAgent);
-                server.readUSBThenStart()
+                server.lockSession(
+                    event.data.sessionId,
+                    event.data.userAgent,
+                );
+                server.readUSBThenStart();
             } catch (e) {
-                postMessage('EXCEPTION: ' + e);
+               self.postMessage('EXCEPTION: ' + e);
             }
-        }
+        };
 
-        self.onerror = function(event) {
-            console.log('ERROR ' + event);
-        }
+        self.onerror = (event) => {
+           self.postMessage('EXCEPTION: ' + event);
+        };
 
-        postMessage('');
-        setImmediate(() => keepAlive());
+        self.postMessage('');
     });
     worker.postMessage({
         serverjs: pserverjs,
@@ -60,21 +58,80 @@ function createServerWorker(pserverjs, plocator, psessionId, puserAgent) {
     return worker;
 }
 
-function workerThreadRestart(code, serverjs, locator, sessionId, ua) {
+function workerThreadRestart(code, serverjs, locator, newSessionId, ua) {
     // exit if main process is gone
     if (!mainWindow) return;
 
     // The worker process does NOT PLAY WELL at all with
     // electron.  We need to keep restarting it.
     // console.log('Server died with code: ' + code + ', restarting');
-    workerThread = createServerWorker(
-        serverjs, locator, sessionId, ua,
-    );
+    workerThread = createServerWorker(serverjs, locator, newSessionId, ua);
 
-    workerThread.child.on('exit', (code) =>
-        workerThreadRestart(
-            code, serverjs, locator, sessionId, ua,
-    ));
+    workerThread.child.on('exit', (ecode) => {
+        workerThreadRestart(ecode, serverjs, locator, newSessionId, ua);
+    });
+}
+
+function findLocator() {
+    const locatorFile = 'locator.json';
+    let found = false;
+    let dir = __dirname;
+    do {
+        if (fs.existsSync(path.join(dir, locatorFile))) {
+            found = true;
+            break;
+        }
+        if (path.dirname(dir) === dir) break;
+        dir = path.resolve(dir, '..');
+    } while (!found);
+
+    if (!found) {
+        throw new Error("can't find locator file: " + locatorFile);
+    }
+
+    // eslint-disable-next-line global-require, import/no-dynamic-require
+    const locator = require(path.join(dir, locatorFile));
+    locator.shared = path.resolve(dir, locator.shared);
+    locator.app = path.resolve(dir, locator.app);
+    locator.drive = path.resolve(dir, locator.drive);
+    // console.log('shared: ' + locator.shared);
+    // console.log('app: ' + locator.app);
+    // console.log('drive: ' + locator.drive);
+
+    return locator;
+}
+
+function onDomReady(win) {
+    // Standard JS injection.
+    // * remove the PDF toolbar to put roadblock against download
+    // * provide callback for opening external URLs in
+    //   the electron browser (insecure)
+    win.webContents.executeJavaScript(`
+        const {ipcRenderer} = require('electron');
+        if (typeof(window.jQuery) === 'undefined') {
+            window.$ = window.jQuery = require('jquery');
+        }
+        $("[data-openlocal='true']").click(function(ev) {
+            ev.preventDefault();
+            // this will prevent triggering the onOpenUrl()
+            // call below.
+            ipcRenderer.send('openlocal-message', ev.target.href);
+        });
+
+        tb = document.querySelector('viewer-pdf-toolbar');
+        if (tb) { tb.style.display = 'none'; }
+    `);
+}
+
+function systemOpenUrl(nurl) {
+    opn(nurl);
+}
+
+function onOpenUrl(ev, nurl) {
+    if (!nurl.match(/^https:\/\/localhost/)) {
+        ev.preventDefault();
+        systemOpenUrl(nurl);
+    }
 }
 
 function createWindow() {
@@ -88,36 +145,39 @@ function createWindow() {
         show: false,
         devTools: false,
         webPreferences: {
-            plugins: true
-        }
+            plugins: true,
+        },
     });
 
     // Start the server in a separate thread.
-    workerThreadRestart(0,
+    workerThreadRestart(
+        0,
         path.join(__dirname, 'server.js'),
         locator,
         sessionId,
         mainWindow.webContents.session.getUserAgent(),
     );
 
-    //mainWindow.webContents.openDevTools();
+    // mainWindow.webContents.openDevTools();
 
     mainWindow
         .webContents.session.webRequest
         .onBeforeSendHeaders((details, callback) => {
-        details.requestHeaders['x-api-key'] =
-            fs.readFileSync(path.join(locator.shared, '.hidfil.sys'))
-              .toString('hex');
-        details.requestHeaders['session-id'] = sessionId;
-        callback({cancel:false, requestHeaders: details.requestHeaders});
-    });
+            const rh = details.requestHeaders;
+            rh['x-api-key'] =
+                fs.readFileSync(path.join(locator.shared, '.hidfil.sys'))
+                  .toString('hex');
+            rh['session-id'] = sessionId;
+            callback({ cancel: false, requestHeaders: rh });
+        });
 
     // ipc connectors
-    electron.ipcMain.on('openlocal-message', (ev, url) => {
-        //console.log('Warning: Opening external URL in browser ' + url);
-        mainWindow.loadURL(url);
+    electron.ipcMain.on('openlocal-message', (ev, nurl) => {
+        // console.log('Warning: Opening external URL in browser ' + url);
+        mainWindow.loadURL(nurl);
     });
     electron.ipcMain.on('getlocator-message', (ev) => {
+        // eslint-disable-next-line no-param-reassign
         ev.returnValue = locator;
     });
 
@@ -125,23 +185,23 @@ function createWindow() {
     mainWindow.loadURL(url.format({
         pathname: path.join(__dirname, 'index.html'),
         protocol: 'file:',
-        slashes: true
+        slashes: true,
     }));
 
     mainWindow.webContents.on('dom-ready', () => onDomReady(mainWindow));
     mainWindow.webContents.on('will-navigate', onOpenUrl);
 
-    mainWindow.webContents.on('new-window', (event, url) => {
+    mainWindow.webContents.on('new-window', (event, nurl) => {
         event.preventDefault();
-        var win = new electron.BrowserWindow({
+        const win = new electron.BrowserWindow({
             width: 800,
             height: 600,
             icon: path.join(__dirname, 'img/appicon.png'),
             webPreferences: {
-                plugins: true
-            }
+                plugins: true,
+            },
         });
-        win.loadURL(url);
+        win.loadURL(nurl);
 
         win.webContents.on('dom-ready', () => onDomReady(win));
         win.webContents.on('will-navigate', onOpenUrl);
@@ -158,75 +218,10 @@ function createWindow() {
     mainWindow.webContents.once('did-finish-load', () => {
         mainWindow.maximize();
         mainWindow.show();
-
     });
 }
 
-function systemOpenUrl(url) {
-    opn(url);
-}
-
-function onDomReady(win) {
-    // Standard JS injection.
-    // * remove the PDF toolbar to put roadblock against download
-    // * provide callback for opening external URLs in
-    //   the electron browser (insecure)
-    win.webContents.executeJavaScript(
-`
-        const {ipcRenderer} = require('electron');
-        if (typeof(window.jQuery) === 'undefined') {
-            window.$ = window.jQuery = require('jquery');
-        }
-        $("[data-openlocal='true']").click(function(ev) {
-            ev.preventDefault();
-            // this will prevent triggering the onOpenUrl()
-            // call below.
-            ipcRenderer.send('openlocal-message', ev.target.href);
-        });
-
-        tb = document.querySelector('viewer-pdf-toolbar');
-        if (tb) { tb.style.display = 'none'; }
-`
-    );
-}
-
-function findLocator() {
-    const locatorFile = 'locator.json';
-    let found = false;
-    let dir = __dirname;
-    do {
-        if (fs.existsSync(path.join(dir, locatorFile))) {
-            found = true;
-            break;
-        }
-        if (path.dirname(dir) == dir) break;
-        dir = path.resolve(dir,'..');
-    } while(!found);
-
-    if (!found) {
-        throw new Error("can't find locator file: " + locatorFile);
-    }
-
-    var locator = require(path.join(dir, locatorFile));
-    locator.shared = path.resolve(dir, locator.shared);
-    locator.app = path.resolve(dir, locator.app);
-    locator.drive = path.resolve(dir, locator.drive);
-    //console.log('shared: ' + locator.shared);
-    //console.log('app: ' + locator.app);
-    //console.log('drive: ' + locator.drive);
-
-    return locator;
-}
-
-function onOpenUrl(ev, url) {
-    if (!url.match(/^https:\/\/localhost/)) {
-        ev.preventDefault();
-        //console.log('Warning: Opening external URL using system ' + url);
-        systemOpenUrl(url);
-    }
-}
-
-let notPrimary = app.makeSingleInstance(() => {
+const notPrimary = app.makeSingleInstance(() => {
     if (mainWindow) {
         if (mainWindow.isMinimized()) {
             mainWindow.restore();
