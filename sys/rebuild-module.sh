286e56eb8527ed31b809ee0e1da77c85a91a4a5b
montedev0516 <montecristodev0516@gmail.com>
2020-04-10 01:02:16 -0400

    upgraded node-gyp and module build script

--- sys/rebuild-module.sh
+++ sys/rebuild-module.sh
@@ -1,5 +1,7 @@
 #../../sys/node_modules/.bin/node-gyp rebuild --target=1.8.2 --arch=x64 --dist-url=https://atom.io/download/electron
-../.bin/node-gyp rebuild --target=1.8.2 --arch=ia32 --dist-url=https://atom.io/download/electron
+mv binding.gyp.old binding.gyp
+../.bin/node-gyp rebuild --target=3.0.13 --arch=ia32 --dist-url=https://www.electronjs.org/headers --build-from-source
+mv binding.gyp binding.gyp.old
 
 # also could be useful:
 # export VCTargetsPath='C:\Program Files\Microsoft Visual Studio\2017\Community\Common7\IDE\VC\VCTargets'
