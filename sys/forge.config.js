bbab3c1d917532e68aac5814125d0642196cca3b
montedev0516 <montecristodev0516@gmail.com>
2023-12-05 23:06:25 -0500

    added hook to remove bad node-gyp dir before packaging

---
+++ sys/forge.config.js
@@ -0,0 +1,41 @@
+const path = require('path')
+const fs = require('node:fs/promises');
+
+module.exports = {
+  packagerConfig: {
+    asar: true,
+  },
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {},
+    },
+  ],
+  plugins: [
+  ],
+  hooks: {
+		packageAfterPrune: async (_config, buildPath) => {
+       const gypPath = path.join(
+         buildPath,
+         'node_modules',
+         'usb-detection',
+         'build',
+         'node_gyp_bins'
+       );
+       await fs.rm(gypPath, {recursive: true, force: true});
+    }
+  }
+};
